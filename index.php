bbd3566af1976df9b99e072b2790f1c3c56008e1
Timothée Poux <timothee.poux@gmail.com>
2025-03-26 19:17:00 +0100

    affichage des classes

--- index.php
+++ index.php
@@ -1,13 +1,23 @@
 <?php
     require_once(__DIR__.'/exo9-tp/Model/pdo.php');
 
-    echo "<br>Partie 2:<br>1.<br>";
+    echo "<br>Partie 2:<br>1.";
     $findeleves = $dbPDO->prepare("SELECT nom,prenom FROM etudiants");
     $findeleves->execute();
     $eleves = $findeleves->fetchAll(PDO::FETCH_CLASS);
     echo '<ul>';
     foreach($eleves as $eleve) {
         echo '<li>'.$eleve->prenom.' '.$eleve->nom.'</li>';
+     }
+     echo '</ul>';
+
+     echo "2.<br>";
+     $findclasses = $dbPDO->prepare("SELECT libelle FROM classes");
+     $findclasses->execute();
+     $classes = $findclasses->fetchAll(PDO::FETCH_CLASS);
+     echo '<ul>';
+    foreach($classes as $classe) {
+        echo '<li>'.$classe->libelle.'</li>';
      }
      echo '</ul>';
 ?>

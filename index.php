95fbbd8da4b924d05b09dc516e1a1badf5739973
Timothée Poux <timothee.poux@gmail.com>
2025-03-26 19:12:31 +0100

    affichage des élèves

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
 <?php
     require_once(__DIR__.'/exo9-tp/Model/pdo.php');
+
+    echo "<br>Partie 2:<br>1.<br>";
+    $findeleves = $dbPDO->prepare("SELECT nom,prenom FROM etudiants");
+    $findeleves->execute();
+    $eleves = $findeleves->fetchAll(PDO::FETCH_CLASS);
+    echo '<ul>';
+    foreach($eleves as $eleve) {
+        echo '<li>'.$eleve->prenom.' '.$eleve->nom.'</li>';
+     }
+     echo '</ul>';
 ?>
